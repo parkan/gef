6c2229ba894c4bceb9a9c64796336d775f4345b0
Arkadiy Kukarkin <arkadiy@mindhole.org>
2015-09-28 16:56:58 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+import { parse }  from 'graphql/language/parser';
+import fs from 'fs-promise';
+import _ from 'lodash';
+require('stackup');
+
+fs.readFile('schema.graphql')
+        .then(body => parse(body))
+        .then(ast => walkAst(ast))
+        .catch(err => console.log(err.stack));
+
+function isNode(fieldDefinitionAst){
+    return fieldDefinitionAst.interfaces
+            && fieldDefinitionAst.interfaces.length
+            && _.chain(fieldDefinitionAst.interfaces).map(i => i.name.value).contains('Node').value()
+}
+
+function extractNodes(definitionsAst){
+    return _.filter(definitionsAst, d => isNode(d));
+}
+
+function extractScalars(definitionsAst){
+    return _.filter(definitionsAst, d => d.kind === 'ScalarTypeDefinition');
+}
+
+function collectFields(objectTypeDefinitionAst){
+    return objectTypeDefinitionAst.fields.map(f => ({
+        [f.name.value] : determineFieldType(f.type) }));
+}
+
+function determineFieldType(typeAst){
+    //console.log(fieldDefinitionAst);
+    switch(typeAst.kind){
+        case 'NonNullType':
+            return { required: true, ...determineFieldType(typeAst.type) };
+            break;
+        case 'NamedType':
+            return { "type": translateType(typeAst.name.value) };
+            break;
+        default:
+            throw new Error("Unknown type kind");
+    }
+}
+
+function translateType(type){
+    return type;
+}
+
+function walkAst(ast){
+    // extract scalars and map to custom Mongoose types
+    // const res = extractScalars(ast.definitions);
+
+    // extract nodes
+    const nodes = extractNodes(ast.definitions);
+
+    // for each node, create a collection
+    const collections = nodes.map(n => collectFields(n));
+    //_.chain(ast.definitions).filter(d => d.interfaces && d.interfaces.length && d.interfaces.map(i => i.name.value).contains('Node')).value();
+    
+    console.log(collections[0]);
+}
